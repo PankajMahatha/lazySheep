67cd5913be413f4ff8dad17bb8a4d62d6edaa98a
toughyear <mahathapankajkumar123@gmail.com>
2021-09-08 09:45:49 +0530

    Git stating

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const app = new express()
 
-app.get('/about',(req,res) => {
+app.get('/about',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'about.html'))
-})
+    })
 
-app.get('/contact',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'contact.html'))
-})
+    app.get('/contact',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'contact.html'))
+        })
 
-app.get('/',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'index.html'))
-})
-
-app.use((req,res) => {
-
-    res.sendFile(path.resolve(__dirname,'notfound.html'))
+    app.get('/post',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'post.html'))
+        })
     
+app.get('/',(req,res)=>{
+res.sendFile(path.resolve(__dirname,'index.html'))
 })
 
 app.listen(3000,() => {
-    console.log('App listening on port 3000')
+    console.log('App Listening on port 3000')
 })
