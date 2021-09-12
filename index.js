8abe1ecdd5a88ad4005a923217cbf54457779733
PankajMahatha <83597644+PankajMahatha@users.noreply.github.com>
2021-09-12 23:10:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = new express()
 const path = require('path')
+const mysql = require('mysql')
 
 app.use(express.static(path.join(__dirname + '/public')))
+
+//mysql connection
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "pankaj",
+    password:"pankaj123@"
+
+})
+
+con.connect((err) => {
+    if(err) throw err
+    console.log('connected')
+})
+
+
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'))
@@ -27,11 +44,31 @@ app.get('/say-hello', (req, res) => {
         data: str,
     });
 });
-
+         
 app.get('/post',(req,res) => {
     res.sendFile(path.join(__dirname + '/public/post.html'))
+})
+
+app.post('/register', (req,res) => {
+    const body = req.body
+    const name = body.name;
+    const email = body.email;
+    const phone = body.phone;
+    const age = body.age;
+    const gender = body.gender;
+
+    const registerUserQuery = "insert into users (name,email,phone,age,gender) values('jam','jam@gmail.com',7658952345,23,'male');"
+    con.query(registerUserQuery,(err,result) => {
+        if(err) throw err
+        console.log(result)
+        res.json({
+            message: "success",
+            result
+        })
+    })
 })
 
 app.listen(3000,() => {
     console.log('App listening on port 3000')
 })
+
